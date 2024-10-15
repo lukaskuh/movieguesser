4c5f8fd55e56d03b209e2c78075a372907b2cb5f
Axel Wennerlund <axel.wennerlund@gmail.com>
2024-10-15 13:56:28 +0200

    Fixed small issue with guess

--- src/AutoCompleter.jsx
+++ src/AutoCompleter.jsx
@@ -7,12 +7,17 @@ export default function AutoCompleter({movies, makeGuess}) {
     const [input, setInput] = useState("");
     console.log("INPUT: " + input);
 
+    function handleClick(event) {
+        makeGuess(currentGuess);
+        setInput("");
+    }
+
     return (
         <div className="fixed flex bottom-2 inset-x-0 flex-col justify-center">
             <AutoList input={input} setCurrentGuess={setCurrentGuess} setInput={setInput} movies={movies} />
             <div className="flex flex-row bg-white w-80">
                 <input value={input} onChange={(event) => setInput(event.target.value)} />
-                <button onClick={() => makeGuess(currentGuess)}>Guess</button>
+                <button onClick={handleClick}>Guess</button>
             </div>
         </div>
     );
