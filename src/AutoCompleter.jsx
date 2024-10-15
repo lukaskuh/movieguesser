03d80c097bdb60814f09de01e682963c999689d2
Axel Wennerlund <axel.wennerlund@gmail.com>
2024-10-15 15:07:12 +0200

    Cooler looking input field

--- src/AutoCompleter.jsx
+++ src/AutoCompleter.jsx
@@ -15,11 +15,11 @@ export default function AutoCompleter({movies, makeGuess}) {
     }
 
     return (
-        <div className="bg-red-500 fixed bottom-2 left-1/2 transform -translate-x-1/2 flex flex-col justify-center w-80">
+        <div className="shadow-sm bg-white rounded fixed bottom-2 left-1/2 transform -translate-x-1/2 flex flex-col justify-center w-80">
             <AutoList input={input} setCurrentGuess={setCurrentGuess} setInput={setInput} focus={focus} setFocus={setFocus} movies={movies} />
             <div className="flex flex-row w-full">
-                <input className="flex-grow" value={input} onChange={(event) => { setInput(event.target.value); setFocus(true); }} />
-                <button onClick={handleClick}>Guess</button>
+                <input className="rounded-l flex-grow" value={input} onChange={(event) => { setInput(event.target.value); setFocus(true); }} />
+                <button className="btn btn-primary !rounded-l-none" onClick={handleClick}>Guess</button>
             </div>
         </div>
     );
