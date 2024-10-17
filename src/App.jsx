d5fecff5826bc2324ec1448591f678b493769d9c
Lukas K <lukaskuhlin@gmail.com>
2024-10-17 09:28:31 +0200

    la till loading state

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import { useState } from 'react'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useNavigation } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.css'
 
 function App() {
+  const navigation = useNavigation();
+  const isLoading = navigation.state === "loading";
 
   return (
-    <Outlet />
+    <>
+      {isLoading 
+      ? (<div className='d-flex h-screen w-screen items-center justify-center'>
+          <p className=''>Loading...</p>
+        </div>)
+      : <Outlet />
+      }
+    </>
   )
 }
 
