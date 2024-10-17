0edc50759d0565f57b4bb73d2349bb85a2f62676
Lukas K <lukaskuhlin@gmail.com>
2024-10-17 09:46:55 +0200

    La till internatioal 2000 i how to play

--- src/HowToPlay.jsx
+++ src/HowToPlay.jsx
@@ -9,11 +9,13 @@ export default function HowToPlay() {
             You have three guesses in total. After the third guess, you lose and will get the correct answer, even if you did not guess the correct movie.
             </p>
             <h3>The categories</h3>
-            <p>There are currently two different game modes:</p>
+            <p>There are currently three different game modes:</p>
+            <h5>International movies - All time</h5> 
+            <p>The movie will be one of the most popular movies of all time, internationally.</p>
+            <h5>International movies - 2000s</h5> 
+            <p>The movie will be one of the most popular movies of the 2000s, internationally.</p>
             <h5>Swedish movies</h5> 
             <p>The movie will be one of Sweden's most popular movies of all time.</p>
-            <h5>International movies</h5> 
-            <p>The movie will be one of the most popular movies of all time, internationally.</p>
             <h3>The hints</h3>
             <h5>Hint 1</h5>
             <p>The first hint will give you the movie's release year, genres and production country.</p>
