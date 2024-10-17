c91768d9ce33c24ab3b8f93a4b5bb40099942a6c
Axel Wennerlund <axel.wennerlund@gmail.com>
2024-10-17 12:33:15 +0200

    Fixed look of the AutoCompleter + added padding

--- src/AutoCompleter.jsx
+++ src/AutoCompleter.jsx
@@ -15,10 +15,10 @@ export default function AutoCompleter({movies, makeGuess}) {
     }
 
     return (
-        <div className="shadow-sm bg-white rounded fixed bottom-2 left-1/2 transform -translate-x-1/2 flex flex-col justify-center w-80">
+        <div className="shadow-md shadow-gray-500/45 bg-white rounded fixed bottom-8 left-1/2 transform -translate-x-1/2 flex flex-col justify-center w-80">
             <AutoList input={input} setCurrentGuess={setCurrentGuess} setInput={setInput} focus={focus} setFocus={setFocus} movies={movies} />
             <div className="flex flex-row w-full">
-                <input className="rounded-l flex-grow px-2" value={input} onChange={(event) => { setInput(event.target.value); setFocus(true); }} />
+                <input className="rounded-l flex-grow px-2 bg-gray-100" value={input} onChange={(event) => { setInput(event.target.value); setFocus(true); }} />
                 <button className="btn btn-primary !rounded-l-none" onClick={handleClick}>Guess</button>
             </div>
         </div>
--- src/HintThree.jsx
+++ src/HintThree.jsx
@@ -3,7 +3,7 @@ import HintContainer from "./HintContainer";
 export default function HintThree({plot}) {
 
     return (
-        <HintContainer className={"max-w-xl"}>
+        <HintContainer className={"max-w-xl pb-20"}>
             <h1>Hint 3</h1>
             <h4>Plot:</h4>
             <p>{plot}</p>
