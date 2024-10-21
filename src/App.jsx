0a83b5178ccb05c5c980f244431d6b915ff2b200
Lukas K <lukaskuhlin@gmail.com>
2024-10-21 23:30:55 +0200

    Navbar and back button

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import { useState } from 'react'
-import { Outlet, useNavigation } from 'react-router-dom'
+import { Outlet, useLocation, useNavigation } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.css'
 
 function App() {
+  const location = useLocation();
   const navigation = useNavigation();
   const isLoading = navigation.state === "loading";
 
+  const isHome = location.pathname === "/";
+
   return (
     <>
+      {!isHome && <header className='d-flex w-screen justify-center py-4'><h3><Link className="no-underline text-inherit" to="/">Guess the Movie</Link></h3></header>}
       {isLoading 
       ? (<div className='d-flex h-screen w-screen items-center justify-center'>
           <p className=''>Loading...</p>
