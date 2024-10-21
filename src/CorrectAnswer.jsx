d56c6a9d25ae3f5b2fd534a48e54731b095bbd19
Lukas K <lukaskuhlin@gmail.com>
2024-10-21 23:02:51 +0200

    Correct answer fix

--- src/CorrectAnswer.jsx
+++ src/CorrectAnswer.jsx
@@ -25,13 +25,13 @@ export default function CorrectAnswer({movies, guessCount}) {
     return (
         <div className="pb-10">
             <h1 className="pb-2">{title()}</h1>
-            <h3>{movie.original_title}</h3>
-            <div>
-                <img className="mb-8 rounded-xl" src={`https://image.tmdb.org/t/p/w200/${movie.poster_path}`} alt={movie.original_title}></img>
-            </div>
-            <div className="d-flex flex-row gap-2">
-                <button className="btn btn-secondary" onClick={handleClick}>Home Page</button>
-                <button className="btn btn-primary" onClick={handleRefresh}>Play again</button>
+            <div className="d-flex flex-col items-center">
+                <h2>{movie.original_title}</h2>
+                <img className="mb-8 rounded-xl w-[300px]" src={`https://image.tmdb.org/t/p/w200/${movie.poster_path}`} alt={movie.original_title}></img>
+                <div className="d-flex flex-row gap-2">
+                    <button className="btn btn-secondary" onClick={handleClick}>Home Page</button>
+                    <button className="btn btn-primary" onClick={handleRefresh}>Play again</button>
+                </div>
             </div>
         </div>
     )
