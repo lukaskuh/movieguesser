6bc706ef36344911bcfd4b9c99eeef2fd8e6d371
Lukas K <lukaskuhlin@gmail.com>
2024-10-22 10:10:56 +0200

    toaster + modal animation

--- src/Play.jsx
+++ src/Play.jsx
@@ -5,13 +5,17 @@ import HintThree from "./HintThree";
 import AutoCompleter from "./AutoCompleter";
 import { useState } from "react";
 import CorrectAnswer from "./CorrectAnswer";
-import { AnimatePresence } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
+
+
 
 export default function Play() {
 
     const data = useLoaderData();
     const [guessCount, setCurrentGuess] = useState(0);
     const [showModal, setShowModal] = useState(false);
+    
+    const [showToaster, setShowToaster] = useState(false);
 
     function makeGuess(guess) {
         if (guess === data.movie.id) {
@@ -21,7 +25,8 @@ export default function Play() {
             if (guessCount === 2) {
                 setShowModal(true);
             } else {
-                alert("False :(");
+                setShowToaster(true);
+                setTimeout(() => setShowToaster(false), 1500);
             }
         }
     }
@@ -32,8 +37,20 @@ export default function Play() {
     console.log(data.crew);
 
     return (
-        <div className="p-3 d-flex flex-col items-center justify-center w-full margin mx-auto bg-gray-400">
+        <div className="p-3 d-flex flex-col items-center justify-center w-full margin mx-auto">
             <AnimatePresence>
+                {showToaster && (
+                    <motion.div
+                        key="toast"
+                        id="toast"
+                        initial={{ y: -50, opacity: 1 }}
+                        animate={{ y: 0, opacity: 1 }}
+                        exit={{opacity: 0 }}
+                        className="fixed top-5 left-1/2 transform -translate-x-1/2 bg-red-700 text-white px-4 py-2 rounded-lg shadow-lg"
+                    >
+                        Wrong!
+                    </motion.div>
+                )}
                 <HintOne releaseYear={data.movie.release_date} genre={data.movie.genres} countries={data.movie.origin_country} key={1}/>
                 {guessCount >= 1 && <HintTwo directors={data.crew.directors} cast={data.crew.cast} key={2} />}
                 {guessCount >= 2 && <HintThree plot={data.movie.overview} key={3} />}
@@ -41,19 +58,19 @@ export default function Play() {
             <AutoCompleter movies={data.movies} makeGuess={makeGuess} />
 
             {showModal && (
-                <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
-                    <div className="relative bg-white p-6 rounded-lg shadow-lg max-w-md w-full">
+                <motion.div initial={{opacity:0}} animate={{opacity:1}} className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+                    <motion.div initial={{y: 500}} animate={{y:0}} className="relative bg-white p-6 rounded-lg shadow-lg max-w-md w-full">
 
                         <button
-                            className="absolute top-2 right-2 text-gray-500 hover:text-gray-700 text-3xl p-2"
+                            className="absolute bg-transparent top-2 right-2 text-gray-500 hover:text-gray-700 text-3xl p-2"
                             onClick={() => setShowModal(false)}
                         >
                             &times;
                         </button>
 
                         <CorrectAnswer guessCount={guessCount} />
-                    </div>
-                </div>
+                    </motion.div>
+                </motion.div>
             )}
 
         </div>
