a89ee05be6dd5dcb39d813d4e83485779a54863c
Lukas K <lukaskuhlin@gmail.com>
2024-10-22 10:38:13 +0200

    placeholder image fix

--- src/Person.jsx
+++ src/Person.jsx
@@ -6,7 +6,7 @@ const item = {
 }
 
 export default function Person({ name, profilePath }) {
-    const defaultImage = "https://www.themoviedb.org/assets/2/v4/glyphicons/basic/glyphicons-basic-19-user-grey-6c757d9bb462cf7b3ba3a9a0a807eb2b8e1b6f3a4a2a2834e811c54f77c987f2.svg";
+    const defaultImage = "https://www.themoviedb.org/assets/2/v4/glyphicons/basic/glyphicons-basic-4-user-grey-d8fe957375e70239d6abdd549fd7568c89281b2179b5f4470e2e12895792dfa5.svg";
 
     return (
         <motion.div className="flex flex-col items-center" variants={item}>
